cb60c5db5dff8edfaa0cdc41cdd80a85829e07a4
H4R7N <75640577+H4R7N@users.noreply.github.com>
2021-01-21 19:57:44 +0300

    Add files via upload

---
+++ sikikbypass.lua
@@ -0,0 +1,470 @@
+local yarak = game.Workspace.Part
+yarak.Name = "Part1"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part2"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part3"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part4"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part5"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part6"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part7"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part8"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part9"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part10"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part11"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part12"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part13"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part14"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part15"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part16"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part17"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part18"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part19"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part20"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part21"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part22"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part23"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part24"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part25"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part26"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part27"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part28"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part29"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part30"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part31"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part32"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part33"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part34"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part35"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part36"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part37"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part38"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part39"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part40"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part41"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part42"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part43"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part44"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part45"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part46"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part47"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part48"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part49"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part50"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part51"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part52"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part53"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part54"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part55"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part56"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part57"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part58"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part59"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part60"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part61"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part62"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part63"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part64"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part65"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part66"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part67"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part68"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part69"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part70"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part71"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part72"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part73"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part74"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part75"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part76"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part77"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part78"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part79"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part80"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part81"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part82"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part83"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part84"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part85"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part86"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part87"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part88"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part89"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part90"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part91"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part92"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part93"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part94"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part95"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part96"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part97"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part98"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part99"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part100"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part101"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part102"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part103"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part104"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part105"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part106"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part107"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part108"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part109"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part110"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part111"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part112"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part113"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part114"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part115"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part116"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part117"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part118"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part119"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part120"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part121"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part122"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part123"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part124"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part125"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part126"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part127"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part128"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part129"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part130"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part131"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part132"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part133"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part134"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part135"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part136"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part137"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part138"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part139"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part140"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part141"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part142"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part143"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part144"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part145"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part146"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part147"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part148"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part149"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part150"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part151"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part152"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part153"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part154"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part155"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part156"
+wait(0.1)
+local yarak = game.Workspace.Part
+yarak.Name = "Part157"
