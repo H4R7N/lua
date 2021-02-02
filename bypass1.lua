09b926c6a5c92f8973ca40395326dc7d4f21ba58
H4R7N <75640577+H4R7N@users.noreply.github.com>
2021-02-02 21:09:25 +0300

    Update bypass1.lua

--- bypass1.lua
+++ bypass1.lua
@@ -480,3 +480,297 @@ yarak.Name = "Part160"
 wait(0.1)
 local yarak = game.Workspace.Block
 yarak.Name = "Part161"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part162"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part163"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part164"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part165"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part166"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part167"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part168"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part169"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part170"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part171"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part172"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part173"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part174"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part175"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part176"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part177"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part178"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part179"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part180"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part181"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part182"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part183"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part184"
+wait(0.1)
+local yarak = game.Workspace.Truss
+yarak.Name = "Part185"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part186"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part187"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part189"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part190"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part191"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part192"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part193"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part194"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part195"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part196"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part197"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part198"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part199"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part200"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part201"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part202"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part203"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part204"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part205"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part206"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part207"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part208"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part209"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part210"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part211"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part212"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part213"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part214"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part215"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part216"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part217"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part218"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part219"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part220"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part221"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part222"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part223"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part224"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part225"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part226"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part227"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part228"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part229"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part230"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part231"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part232"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part233"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part234"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part235"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part236"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part237"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part238"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part239"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part240"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part241"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part242"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part243"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part244"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part245"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part246"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part247"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part248"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part249"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.KillBlock
+yarak.Name = "Part250"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.Part
+yarak.Name = "Part251"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.Part
+yarak.Name = "Part252"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.Part
+yarak.Name = "Part253"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.Part
+yarak.Name = "Part254"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.Part
+yarak.Name = "Part255"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.Part
+yarak.Name = "Part256"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.Part
+yarak.Name = "Part257"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.Part
+yarak.Name = "Part258"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.Part
+yarak.Name = "Part259"
+wait(0.1)
+local yarak = game.Workspace.KillBlock.Part
+yarak.Name = "Part260"
