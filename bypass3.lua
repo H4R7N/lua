486dac7334bf24bb52a30fc9229c7da1fc9ee9f6
H4R7N <75640577+H4R7N@users.noreply.github.com>
2021-02-02 19:13:14 +0300

    Add files via upload

---
+++ bypass3.lua
@@ -0,0 +1,73 @@
+game.StarterGui:SetCore("SendNotification",{["Title"]="H4R7N Hack",["Text"]="Bypass 3 Yapiliyor Lutfen Bekleyiniz!",["Duration"]=4})
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss1"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss2"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss3"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss4"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss5"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss6"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss7"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss8"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss9"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss10"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss11"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss12"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss13"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss14"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss15"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss16"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss17"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss18"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss19"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss20"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss21"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss22"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss23"
+wait(0.1)
+local yarak2 = game.Workspace.Truss
+yarak2.Name = "Truss24"
