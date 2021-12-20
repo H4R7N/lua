af0428bcf5e6602b2f605068b7116d827caedebb
H4R7N <75640577+H4R7N@users.noreply.github.com>
2021-12-20 20:52:39 +0300

    Add files via upload

---
+++ JTOH H4R7N Hack Loader.lua
@@ -0,0 +1,6 @@
+setclipboard("yarra")
+game.StarterGui:SetCore("SendNotification",{["Title"]="Tutorial!",["Text"]="The video of the cheat has been copied to the clipboard!",["Duration"]=20,["Button1"]="OK!"})
+
+if game.PlaceId == 2920068706 or game.PlaceId == 2921149558 or game.PlaceId == 2921150129 or game.PlaceId == 2921151152 or game.PlaceId == 2921153965 or game.PlaceId == 2921154244 or game.PlaceId == 2921154946 or game.PlaceId == 2921155105 or game.PlaceId == 2921155228 or game.PlaceId == 2942729079 or game.PlaceId == 2942730116 or game.PlaceId == 3712200731 or game.PlaceId == 3712201724 or game.PlaceId == 3712202960 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/H4R7N/lua/main/JTOH%20H4R7N%20Hack.lua"))()
+end
