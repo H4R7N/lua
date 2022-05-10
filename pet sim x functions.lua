63e6dbb56339a42f39e2ccaa0b7ac37bb7a71c3a
H4R7N <75640577+H4R7N@users.noreply.github.com>
2022-05-10 18:39:48 +0300

    Add files via upload

---
+++ pet sim x functions.lua
@@ -0,0 +1,181 @@
+local args = {
+    [1] = "a",
+    [2] = "player teleported"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "b",
+    [2] = "request cannon launch"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "a",
+    [2] = "cannon fire"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "a",
+    [2] = "cannon fired"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "b",
+    [2] = "redeem rank rewards"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "a",
+    [2] = "rewards redeemed"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "b",
+    [2] = "join coin"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "a",
+    [2] = "farm coin"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "a",
+    [2] = "open egg"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "a",
+    [2] = "notification"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "b",
+    [2] = "travel to trading plaza"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "b",
+    [2] = "travel to main world"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "b",
+    [2] = "redeem merch code"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "b",
+    [2] = "redeem pet collection"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "b",
+    [2] = "redeem twitter code"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "b",
+    [2] = "redeem vip rewards"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "b",
+    [2] = "get coins"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "a",
+    [2] = "request world"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "b",
+    [2] = "buy egg"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "a",
+    [2] = "claim orb"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "a",
+    [2] = "performed teleport"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "a",
+    [2] = "activate boost"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "b",
+    [2] = "bank deposit"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "b",
+    [2] = "bank withdraw"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "b",
+    [2] = "redeem free gift"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
+
+local args = {
+    [1] = "a",
+    [2] = "boost ended"
+}
+
+workspace.__THINGS.__REMOTES.MAIN:FireServer(unpack(args))
