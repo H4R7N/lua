fc27c7adac9ebb17f16525e55ac0a9b0bb7ff91d
H4R7N <75640577+H4R7N@users.noreply.github.com>
2022-07-27 20:42:51 +0300

    Update victoryraceautowin.lua

--- victoryraceautowin.lua
+++ victoryraceautowin.lua
@@ -1,124 +1,119 @@
-game.Players.LocalPlayer.Character.Humanoid.Health = 0
-function orospu()
-    wait(2)
+game.StarterGui:SetCore("SendNotification",{["Title"]="H4R7N Hack",["Text"]="Bypassing...",["Duration"]=2})
     	local killparts = game.workspace:GetDescendants()
 
 	for i,v in pairs (killparts) do
 		if v.ClassName == "TextLabel" then
 		    if v.Text == "1" then
 		        v.Parent.Parent.Name = "Win1"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win1, 0)
+			wait(0.1)
 		    elseif v.Text == "2" then
 		        v.Parent.Parent.Name = "Win2"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win2, 0)
+			wait(0.1)
 		    elseif v.Text == "3" then
 		        v.Parent.Parent.Name = "Win3"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win3, 0)
+			wait(0.1)
 		    elseif v.Text == "4" then
 		        v.Parent.Parent.Name = "Win4"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win4, 0)
+			wait(0.1)
 		    elseif v.Text == "5" then
 		        v.Parent.Parent.Name = "Win5"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win5, 0)
+			wait(0.1)
 		    elseif v.Text == "6" then
 		        v.Parent.Parent.Name = "Win6"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win6, 0)
+			wait(0.1)
 		    elseif v.Text == "7" then
 		        v.Parent.Parent.Name = "Win7"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win7, 0)
+			wait(0.1)
 		    elseif v.Text == "8" then
 		        v.Parent.Parent.Name = "Win8"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win8, 0)
+			wait(0.1)
 		    elseif v.Text == "9" then
 		        v.Parent.Parent.Name = "Win9"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win9, 0)
+			wait(0.1)
 		    elseif v.Text == "10" then
 		        v.Parent.Parent.Name = "Win10"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win10, 0)
+			wait(0.1)
 		    elseif v.Text == "11" then
 		        v.Parent.Parent.Name = "Win11"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win11, 0)
+			wait(0.1)
 		    elseif v.Text == "12" then
 		        v.Parent.Parent.Name = "Win12"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win12, 0)
+			wait(0.1)
 		    elseif v.Text == "13" then
 		        v.Parent.Parent.Name = "Win13"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win13, 0)
+			wait(0.1)
 		    elseif v.Text == "14" then
 		        v.Parent.Parent.Name = "Win14"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win14, 0)
+			wait(0.1)
 		    elseif v.Text == "15" then
 		        v.Parent.Parent.Name = "Win15"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win15, 0)
+			wait(0.1)
 		    elseif v.Text == "16" then
 		        v.Parent.Parent.Name = "Win16"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win16, 0)
+			wait(0.1)
 		    elseif v.Text == "17" then
 		        v.Parent.Parent.Name = "Win17"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win17, 0)
+			wait(0.1)
 		    elseif v.Text == "18" then
 		        v.Parent.Parent.Name = "Win18"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win18, 0)
+			wait(0.1)
 		    elseif v.Text == "19" then
 		        v.Parent.Parent.Name = "Win19"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win19, 0)
+			wait(0.1)
 		    elseif v.Text == "20" then
 		        v.Parent.Parent.Name = "Win20"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win20, 0)
+			wait(0.1)
 		    elseif v.Text == "21" then
 		        v.Parent.Parent.Name = "Win21"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win21, 0)
+			wait(0.1)
 		    elseif v.Text == "22" then
 		        v.Parent.Parent.Name = "Win22"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win22, 0)
+			wait(0.1)
 		    elseif v.Text == "23" then
 		        v.Parent.Parent.Name = "Win23"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win23, 0)
+			wait(0.1)
 		    elseif v.Text == "24" then
 		        v.Parent.Parent.Name = "Win24"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win24, 0)
+			wait(0.1)
 		    elseif v.Text == "25" then
 		        v.Parent.Parent.Name = "Win25"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win25, 0)
+			wait(0.1)
 		    elseif v.Text == "50" then
 		        v.Parent.Parent.Name = "Win50"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win50, 0)
+			wait(0.1)
 		    elseif v.Text == "100" then
 		        v.Parent.Parent.Name = "Win100"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win100, 0)
+			wait(0.1)
 		    elseif v.Text == "250" then
 		        v.Parent.Parent.Name = "Win250"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win250, 0)
+			wait(0.1)
 		    elseif v.Text == "500" then
 		        v.Parent.Parent.Name = "Win500"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win500, 0)
+			wait(0.1)
 		    elseif v.Text == "1k" then
 		        v.Parent.Parent.Name = "Win1k"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win1k, 0)
+			wait(0.1)
 		    elseif v.Text == "2k" then
 		        v.Parent.Parent.Name = "Win2k"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win2k, 0)
+			wait(0.1)
 		    elseif v.Text == "5k" then
 		        v.Parent.Parent.Name = "Win5k"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win5k, 0)
+			wait(0.1)
 		    elseif v.Text == "10k" then
 		        v.Parent.Parent.Name = "Win10k"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win10k, 0)
+			wait(0.1)
 		    elseif v.Text == "20k" then
 		        v.Parent.Parent.Name = "Win20k"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win20k, 0)
+			wait(0.1)
 		    elseif v.Text == "50k" then
 		        v.Parent.Parent.Name = "Win50k"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win50k, 0)
+			wait(0.1)
 		    elseif v.Text == "100k" then
 		        v.Parent.Parent.Name = "Win100k"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Win100k, 0)
+			wait(0.1)
 		    elseif v.Text == "End" then
 		        v.Parent.Parent.Name = "WinEnd"
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").WinEnd, 0)
-		        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Resets, 0)
 		    end
 		end
 	end
-end
-game.Players.LocalPlayer.CharacterAdded:Connect(orospu)
+game.StarterGui:SetCore("SendNotification",{["Title"]="H4R7N Hack",["Text"]="Bypassed.",["Duration"]=2})
